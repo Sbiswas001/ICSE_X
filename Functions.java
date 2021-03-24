234626affab8739a121e08abd7544eccb52c44d0
Tech Friends <55659614+TechFriends1234@users.noreply.github.com>
2021-03-24 12:28:06 +0530

    new

--- Functions.java
+++ Functions.java
@@ -140,4 +140,219 @@ public class Functions
         int s = (int) Math.sqrt(x);
         return (s*s == x);
     }
+    public int[] asort_int(int a[])
+    {
+        int temp=0;
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        return a;  
+    }
+    public int[] dsort_int(int a[])
+    {
+        int temp=0;
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]<a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        return a;  
+    }
+    
+    
+    
+    
+    public long factorial(long a)
+    {
+        long p=1;
+        for(long i=1;i<=a;i++)
+        p*=i;
+        return p;
+    }
+    public long reverse(long n)
+    {
+        long sum=0, r=0; 
+        while(n>0)
+        {    
+            r=n%10; 
+            sum=(sum*10)+r;    
+            n=n/10;    
+        }
+        return sum;
+    }
+    public long array_sum(long a[])
+    {
+        long n=a.length, sum=0;
+        for(long i=0;i<n;i++)
+        {
+            sum+=a[(int)i];
+        }
+        return sum;
+    }
+    public long digit_sum(long n)
+    {
+        long sum=0, r=0;
+        while(n>0)
+        {    
+            r=n%10; 
+            sum+=r;    
+            n=n/10;    
+        }
+        return sum;
+    }
+    public long number_of_digits(long n)
+    {
+        long length = 0;  
+        while(n != 0)
+        {  
+            length++;  
+            n = n/10;  
+        }  
+        return length;
+    }
+    public long[] digit_array(long a)
+    {
+        long x[] = new long[(int)number_of_digits(a)];
+        long num=reverse(a);
+        for(long i=0;i<number_of_digits(a);i++)
+        {
+            x[(int)i]=num%10;
+            num/=10;
+        }
+        return x;
+    }
+    public long no_of_factors(long n)
+    {
+        long count=1;
+        if(n>=2)
+        {
+            for(long i=2;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public long[] factors(long n)
+    {
+        long l=1;
+        long num[] = new long[(int)no_of_factors(n)];
+        num[0]=1;
+        if(n>=2)
+        {
+            for(long i=2;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    num[(int)l]=i;
+                    l++;
+                }
+            }
+        }
+        return num;
+    }
+    public long no_of_pfactors(long n)
+    {
+        long count=0;
+        Prime_Number ob1 = new Prime_Number();
+        if(n>=2)
+        {
+            for(long i=2;i<=n;i++)
+            {
+                if(ob1.prime_check((int)i))
+                {
+                    while(n%i==0)
+                    {
+                        n=n/i;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    public long[] prime_factors(long n)
+    {
+        long l=0;
+        Prime_Number ob1 = new Prime_Number();
+        long num[] = new long[(int)no_of_pfactors(n)];
+        if(n>=2)
+        {
+            for(long i=2;i<=n;i++)
+            {
+                if(ob1.prime_check((int)i))
+                {
+                    while(n%i==0)
+                    {
+                        n=n/i;
+                        num[(int)l]=i;
+                        l++;
+                    }
+                }
+            }
+        }
+        return num;
+    }
+    public boolean isPerfectSquare(long x)
+    {
+        long s = (long) Math.sqrt(x);
+        return (s*s == x);
+    }
+    public long[] asort_long(long a[])
+    {
+        long temp=0;
+        long n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        return a;  
+    }
+    public long[] dsort_long(long a[])
+    {
+        long temp=0;
+        long n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]<a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        return a;  
+    }
 }
--- ISBN.java
+++ ISBN.java
@@ -1,13 +1,13 @@
 public class ISBN
 {
-    public boolean isbn_check(int n)
+    public boolean isbn_check(long n)
     {
         Functions ob1 = new Functions();
-        int x[]=ob1.digit_array(n);
-        int sum=0;
+        long x[]=ob1.digit_array(n);
+        long sum=0;
         for(int i=0;i<x.length;i++)
         {
-            sum+=i*x[i];
+            sum+=(i+1)*x[i];
         }
         if(sum%11==0)
             return true;
